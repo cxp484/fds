c8de905670c421a7b52d91f10768cd840b0af712
Chandan Paul <paulchandan19@gmail.com>
2025-06-16 14:07:25 -0400

    FDS Source: Add str2bool

--- Utilities/Python/scripts/cantera_yaml_2_fds.py
+++ Utilities/Python/scripts/cantera_yaml_2_fds.py
@@ -5,6 +5,17 @@ import pandas as pd
 import sys
 import math
 
+# Define str2bool function
+def str2bool(v):
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
 parser = argparse.ArgumentParser()
 
 parser.add_argument("--background", help="Background species formula",default='noinput',required=True)
